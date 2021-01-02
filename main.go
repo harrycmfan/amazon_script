8f3816abd5a032b6554c5e997308c6ca2d7b5437
LAN Qi <harrycmfan@qq.com>
2021-01-02 09:34:17 +0800

    Update main.go

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
                                                                         },
                                                                         "msgtype": "text"
                                                                 }`)
-                        _, _ = http.Post("https://oapi.dingtalk.com/robot/send?access_token=d5d3573928d71671cca8174e90b29200fe06628b2f357c5663163c6a0127517e", "application/json", bytes.NewBuffer(jsonStr))
+                        _, _ = http.Post("https://oapi.dingtalk.com/robot/send?access_token=your token", "application/json", bytes.NewBuffer(jsonStr))
                 }
         }()
         count := 448439
@@ -55,7 +55,7 @@ r=8-1&th=1")
                                                                         },
                                                                         "msgtype": "text"
                                                                 }`)
-                        _, _ = http.Post("https://oapi.dingtalk.com/robot/send?access_token=d5d3573928d71671cca8174e90b29200fe06628b2f357c5663163c6a0127517e", "application/json", bytes.NewBuffer(jsonStr))
+                        _, _ = http.Post("https://oapi.dingtalk.com/robot/send?access_token=your toke", "application/json", bytes.NewBuffer(jsonStr))
                 }
                 
                                resp, err = http.Get("https://www.amazon.sg/Sony-PlayStation-5-DualSense-Wireless-Controller/dp/B08HNSWWT7/ref=sr_1_1?crid=H0ICLGPS30O3&dchild=1&keywords=playstation%2B5&qid=1605700549&sprefix=plays%2Caps%2C343&sr
@@ -81,7 +81,7 @@ r=8-1&th=1")
                                                                         },
                                                                         "msgtype": "text"
                                                                 }`)
-                        _, _ = http.Post("https://oapi.dingtalk.com/robot/send?access_token=d5d3573928d71671cca8174e90b29200fe06628b2f357c5663163c6a0127517e", "application/json", bytes.NewBuffer(jsonStr))
+                        _, _ = http.Post("https://oapi.dingtalk.com/robot/send?access_token=your token", "application/json", bytes.NewBuffer(jsonStr))
                 }
                 count++
                 fmt.Println("已运行", count, "次")
